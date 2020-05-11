bb3881090ae4ec6dad7e82a7834eb08f5c09106b
AntonioCS <antoniocs@gmail.com>
2020-05-11 22:37:57 +0100

    Added getElementById

--- src/HtmlParser/Document.cpp
+++ src/HtmlParser/Document.cpp
@@ -1,1 +1,28 @@
 ﻿#include "Document.h"
+
+namespace HtmlParser
+{
+
+    Element* Document::getElementById(std::string_view id)
+    {
+        return getElementById(m_root.getChildren(), id);
+    }
+
+    Element* Document::getElementById(std::vector<Element>& elements, std::string_view id)
+    {
+        for (auto& element : elements) {
+            if (element.getId() == id) {
+                return &element;
+            }
+
+            if (element.hasChildren()) {
+                if (auto res = getElementById(element.getChildren(), id);
+                    res) {
+                    return res;
+                }
+            }
+        }
+
+        return nullptr;
+    }
+}
