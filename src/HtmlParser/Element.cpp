927b6cae746e67ed756622dc570d3848948eda8e
AntonioCS <antoniocs@gmail.com>
2020-05-13 16:23:43 +0100

    Implemented many methods

--- src/HtmlParser/Element.cpp
+++ src/HtmlParser/Element.cpp
@@ -1,15 +1,7 @@
 ﻿#include "Element.h"
-#include "Util.h"
 
 namespace HtmlParser
 {
-    Element::~Element()
-    {
-        for (auto child : m_children) {
-            free(child);
-        }
-    }
-
     Element* Element::getParent() const
     {
         return m_parent;
@@ -22,31 +14,226 @@ namespace HtmlParser
 
     void Element::processAttributeString(const std::string& attributesStr)
     {
-        auto allBySpace = Util::split(attributesStr, ' ');
-        for (const auto& keyValue : allBySpace) {
-            auto res = Util::split(keyValue, '=');
-            //m_attributes.
+        AttributeStage stage{ AttributeStage::Invalid };
+        char valueQuotes{ '"' };
+        std::string key{};
+        std::string value{};
+
+        for (std::size_t i{}; i < attributesStr.size(); i++) {
+            const auto c = attributesStr[i];
+
+            switch (stage) {
+                case AttributeStage::Invalid:
+                    if (c != ' ') {
+                        key = c;
+                        stage = AttributeStage::Attr;
+                    }
+                break;
+                case AttributeStage::Attr:
+                {
+                    switch (c) {
+                        case '=':
+                            stage = AttributeStage::ValueEntryPoint;
+                        break;
+                        case ' ':
+                            stage = AttributeStage::Assign;
+                        break;
+                        default:
+                            key += c;
+                    }
+                }
+                break;
+                case AttributeStage::ValueEntryPoint:
+                {
+                    if (c == '"' || c == '\'') {
+                        valueQuotes = c;
+                        stage = AttributeStage::ValueQuotes;
+                    }
+                    else if (c != ' ') {
+                        value = c;
+                        stage = AttributeStage::ValueNaked;
+                    }
+                }
+                break;
+                case AttributeStage::ValueNaked:
+                {
+                    if (c != ' ')
+                        value += c;
+                    else
+                        stage = AttributeStage::Assign;
+                }
+                break;
+                case AttributeStage::ValueQuotes:
+                {
+                    auto f = attributesStr.find(valueQuotes, i);
+                    if (f == std::string::npos) {
+                        f = attributesStr.size() - 1;
+                    }
+
+                    value = attributesStr.substr(i, f - i);
+                    stage = AttributeStage::Assign;
+                    i = f; //move i to the same position as the 'valueQuotes' char
+
+                    if (i + 1 >= attributesStr.size())
+                        i -= 2;
+                }
+                break;
+                case AttributeStage::Assign:
+                {
+                    if (!key.empty()) {
+                        m_attributes[key] = value;
+
+                        key.clear();
+                        value.clear();
+
+                        stage = AttributeStage::Invalid;
+                        i--;
+                    }
+                }
+                break;
+            }
         }
     }
 
-    void Element::addChild(Element* child)
+    void Element::addChild(Element&& child)
     {
-        child->setParent(this);
+        child.setParent(this);
         m_children.push_back(child);
     }
 
-    const std::string& Element::getElementName() const
+    std::vector<Element>& Element::getChildren()
     {
-        return m_name;
+        return m_children;
     }
 
-    std::string Element::getInnerText() const
+    std::string_view Element::getElementName() const
     {
-        return m_inner_text;
+        return std::string_view{ m_name };
     }
 
-    void Element::setInnerText(const std::string& innerText)
+    std::string Element::getInnerHtml() const
     {
-        m_inner_text = innerText;
+        return m_inner_html;
+    }
+
+    void Element::setInnerHtml(const std::string& innerText)
+    {
+        m_inner_html = innerText;
+    }
+
+    bool Element::hasAttributes() const
+    {
+        return !m_attributes.empty();
+    }
+
+    bool Element::hasAttribute(const std::string& attr) const
+    {
+        return (m_attributes.find(attr) != m_attributes.end());
+    }
+
+    std::string Element::attr(const std::string& name)
+    {
+        if (hasAttribute(name))
+            return m_attributes[name];
+
+        return "";
+    }
+
+    void Element::attr(const std::string& name, std::string value)
+    {
+        m_attributes[name] = std::move(value);
+    }
+
+    bool Element::hasChildren() const
+    {
+        return !m_children.empty();
+    }
+
+    Element* Element::getFirstChild()
+    {
+        if (!m_children.empty())
+            return &m_children.front();
+        return nullptr;
+    }
+
+    Element* Element::getLastChild()
+    {
+        if (!m_children.empty())
+            return &m_children.back();
+        return nullptr;
+    }
+
+    Element* Element::getNthChild(std::size_t n)
+    {
+        if (!m_children.empty() && n < m_children.size())
+            return &m_children.at(n);
+        return nullptr;
+    }
+
+    std::size_t Element::getChildrenCount() const
+    {
+        return m_children.size();
+    }
+
+    std::size_t Element::getTotalChildrenCount() const
+    {
+        std::size_t total{};
+
+        if (!m_children.empty()) {
+            for (const auto& child : m_children) {
+                total++;
+
+                if (child.hasChildren())
+                    total += child.getTotalChildrenCount();
+            }
+        }
+
+        return total;
+    }
+
+    std::string Element::toString() const
+    {
+        if (m_name.empty()) {
+            return m_inner_html;
+        }
+
+        std::string output{ "<" + m_name + toStringAttrList() + ">\n"};
+
+        if (m_type == Type::NORMAL) {
+            for (const auto& child : m_children) {
+                output += "\t" + child.toString();
+            }
+
+            output += "\n</" + m_name + ">\n";
+        }
+
+        return output;
+    }
+
+    std::string Element::toStringAttrList() const
+    {
+        std::string output{};
+
+        if (!m_attributes.empty()) {
+            for (const auto& [key, value] : m_attributes) {
+                output += ' ';
+                output += key;
+                output += "=\"";
+                output += value;
+                output += '"';
+            }
+        }
+
+        return output;
+    }
+
+    std::string_view Element::getId() const
+    {
+        auto f = m_attributes.find("id");
+        if (f != m_attributes.end()) {
+            return std::string_view{ f->second };
+        }
+
+        return std::string_view{};
     }
 }
--- src/HtmlParser/Element.h
+++ src/HtmlParser/Element.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
+
 #include <string>
+#include <string_view>
 #include <unordered_map>
 #include <vector>
 
@@ -7,25 +9,109 @@ namespace HtmlParser
 {
     class Element
     {
+    public:
+        enum class Type
+        {
+            TEXT,
+            SELF_CLOSE,
+            NORMAL
+        };
+
+        const std::string tagName;
+    private:
+        Type m_type{ Type::NORMAL };
         Element* m_parent{ nullptr };
         std::string m_name{};
-        std::vector<Element*> m_children{};
+        std::vector<Element> m_children{};
         std::unordered_map<std::string, std::string> m_attributes{};
-        std::string m_inner_text;
+        std::string m_inner_html{};
+
+        enum class AttributeStage {
+            Attr,
+            ValueEntryPoint,
+            ValueQuotes,
+            ValueNaked,
+            Assign,
+            Invalid
+        };
     public:
         Element() = default;
-        Element(const std::string name) : Element(nullptr, name) {}
-        Element(Element *parent, const std::string name) : m_parent(parent), m_name(name) {}
 
-        virtual ~Element();;
+        Element(const std::string& name) : Element(Type::NORMAL, nullptr, name) {}
+
+        Element(const std::string& name, const std::string& attributes) : Element(Type::NORMAL, nullptr, name, attributes) {}
+
+        Element(Element* parent, const std::string& name) : Element(Type::NORMAL, parent, name) {}
+
+        Element(Element* parent, const std::string& name, const std::string& attributes) : Element(Type::NORMAL, parent, name, attributes) {}
+
+        Element(Type type, const std::string& nameOrData) : Element(type, nullptr, nameOrData) {}
+
+        Element(Type type, Element* parent, const std::string& nameOrData) : Element(type, parent, nameOrData, "") {}
+
+        Element(Type type, const std::string& name, const std::string& attributes) : Element(type, nullptr, name, attributes) {}
+
+        Element(Type type, Element* parent, const std::string& nameOrData, const std::string& attributes) : tagName(type == Type::TEXT ? "" : nameOrData), m_type(type), m_parent(parent), m_name(nameOrData){
+            if (!attributes.empty())
+                processAttributeString(attributes);
+
+            if (type == Type::TEXT) {
+                m_name.clear();
+                m_attributes.clear();
+
+                m_inner_html = nameOrData;
+            }
+        }
+
+        //Following constructors don't need type as it will always be the default of NORMAL
+        Element(const std::string& name, Element&& child) : Element(nullptr, name, "", std::move(child)) {}
+
+        Element(const std::string& name, const std::string& attributes, Element&& child) : Element(nullptr, name, attributes, std::move(child)) {}
+
+        Element(Element* parent, const std::string& name, const std::string& attributes, Element&& child) : Element(parent, name, attributes, std::vector<Element>{ child }) {}
+
+        Element(const std::string& name, std::vector<Element>&& children) : Element(nullptr, name, "", std::move(children)) {}
+
+        Element(const std::string& name, const std::string& attributes, std::vector<Element>&& children) : Element(nullptr, name, attributes, std::move(children)) {}
+
+        Element(Element* parent, const std::string& name, const std::string& attributes, std::vector<Element>&& children)
+            : tagName(name), m_parent(parent), m_name(name)
+        {
+            if (!attributes.empty())
+                processAttributeString(attributes);
+
+            for (auto&& child : children) {
+                addChild(std::move(child));
+            }
+        }
+
+        virtual ~Element() = default;
+
         [[nodiscard]] Element* getParent() const;
         void setParent(Element* p);
+        void addChild(Element&& child);
+        [[nodiscard]] std::vector<Element>& getChildren();
+        [[nodiscard]] std::string_view getElementName() const;
+
+        [[nodiscard]] std::string getInnerHtml() const;
+        void setInnerHtml(const std::string& innerText);
+
+        [[nodiscard]] bool hasAttributes() const;
+        [[nodiscard]] bool hasAttribute(const std::string&) const;
+        [[nodiscard]] std::string attr(const std::string&);
+        void attr(const std::string&, std::string);
+        [[nodiscard]] bool hasChildren() const;
+        [[nodiscard]] Element* getFirstChild();
+        [[nodiscard]] Element* getLastChild();
+        [[nodiscard]] Element* getNthChild(std::size_t n);
+        [[nodiscard]] std::size_t getChildrenCount() const;
+        [[nodiscard]] std::size_t getTotalChildrenCount() const;
+        //@todo: improve output
+        [[nodiscard]] std::string toString() const;
+        [[nodiscard]] std::string toStringAttrList() const;
+        [[nodiscard]] std::string_view getId() const;
+
+    private:
         void processAttributeString(const std::string& attributesStr);
-        void addChild(Element* child);
-        [[nodiscard]] const std::string& getElementName() const;
-
-
-        std::string getInnerText() const;
-        void setInnerText(const std::string& innerText);
     };
 }
