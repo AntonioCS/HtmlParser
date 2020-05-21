a4512ab2877ffae865cade609d1dddd89cb8369d
AntonioCS <antoniocs@gmail.com>
2020-05-21 15:44:31 +0100

    Token collection, no longer use just vector of tokens

---
+++ src/HtmlParser/TokenCollection.cpp
@@ -0,0 +1,106 @@
+﻿#include "TokenCollection.h"
+#include <algorithm> //std::find_if
+#include <iterator> //std::distance
+
+//#define DEBUG_TOKENCOLLECTION
+#ifdef DEBUG_TOKENCOLLECTION
+#include <iostream>
+#include "../tests/TokenizerTestHelper.h"
+#endif
+namespace HtmlParser
+{
+
+    TokenCollection* TokenCollection::add(TokenType type, const std::string& name, const std::string& attr)
+    {
+        std::size_t level{};        
+
+        if (!m_tokens.empty()) {
+            const auto& last_token = m_tokens.back();
+            level = last_token.level;
+
+            if (isOpenType(last_token)) {
+                level += Token::level_amount;
+            }
+
+            if (isCloseType(type)) {
+                level -= Token::level_amount;
+            }
+        }
+        
+        m_tokens.emplace_back(type, name, attr, level);
+
+        return this;
+    }
+
+    void TokenCollection::set(const std::vector<Token>& tokens)
+    {
+        m_tokens = tokens;
+        calculateLevelValues();
+    }
+
+    void TokenCollection::insert(const cit position, const Token& token)
+    {
+        m_tokens.insert(position, token);
+        calculateLevelValues();
+    }
+
+    void TokenCollection::insert(const cit position, const std::vector<Token>& tokens)
+    {
+        m_tokens.insert(position, tokens.begin(), tokens.end());
+        calculateLevelValues();
+    }
+
+    TokenCollection::cit TokenCollection::findTag(cit startingPoint, const TokenSearch& tokenSearch, const FindType mode) const
+    {
+        auto func = [&tokenSearch](const Token& token)
+        {
+            bool ret{ true };
+
+#ifdef DEBUG_TOKENCOLLECTION
+            std::cout << "Current tag inspecting: ";
+            output_token(token);        
+#endif
+
+            if (tokenSearch.level_given && tokenSearch.level != 0)
+                ret = tokenSearch.level == token.level;
+            if (tokenSearch.code_given)
+                ret = tokenSearch.code == token.code && ret;
+            if (tokenSearch.tag_given && ret)
+                ret = tokenSearch.tag == token.tag;
+            if (tokenSearch.type_given && ret)
+                ret = tokenSearch.type == token.type;
+
+            return ret;
+        };
+
+        if (mode == FindType::BEGINNING)
+            return std::find_if<cit>(startingPoint,m_tokens.cend(), func);
+
+        auto spoint = m_tokens.rbegin();
+
+        if (startingPoint != m_tokens.begin())
+            spoint += std::distance <cit>(startingPoint, m_tokens.cend());
+
+        auto res = std::find_if(spoint, m_tokens.rend(), func);
+
+        if (res == m_tokens.rend()) return m_tokens.end();
+        return res.base() - 1;
+    }
+
+    void TokenCollection::calculateLevelValues()
+    {
+        std::size_t level{};
+
+        for (auto& token : m_tokens) {
+            token.level = level;
+
+            if (isOpenType(token)) {
+                level += Token::level_amount;
+            }
+            else if (isCloseType(token)) {
+                level -= Token::level_amount;
+                token.level = level;
+            }
+        }
+    }
+}
---
+++ src/HtmlParser/TokenCollection.h
@@ -0,0 +1,137 @@
+﻿#pragma once
+
+#include <vector>
+#include <HtmlParser/Token.h>
+
+namespace HtmlParser
+{
+    class TokenCollection
+    {
+        std::vector<Token> m_tokens;
+        using it = std::vector<Token>::iterator;
+        using cit = std::vector<Token>::const_iterator;
+
+        class TokenSearch
+        {
+        public:
+            enum class NumValue
+            {
+                LEVEL,
+                CODE
+            };
+
+            TokenType type{};
+            std::string tag{};            
+            std::size_t level{};
+            std::size_t code{};
+        
+            bool type_given{ false };
+            bool tag_given{ false };
+            bool level_given{ false };
+            bool code_given{ false };
+
+            TokenSearch(const TokenType type) : type(type), type_given(true) {}
+            TokenSearch(std::size_t num, NumValue nv = NumValue::LEVEL) {
+                assignCorrectValue(num, nv);
+            }
+            TokenSearch(const std::string& tag) : tag(tag), tag_given(true) {}
+            TokenSearch(const std::string& tag, std::size_t num, NumValue nv = NumValue::LEVEL) : tag(tag), tag_given(true)
+            {
+                assignCorrectValue(num, nv);
+            }
+
+            TokenSearch(const std::string& tag, std::size_t level, std::size_t code) : tag(tag), level(level), code(code), tag_given(true), level_given(true), code_given(true) {}
+            TokenSearch(const TokenType type, const std::string& tag) : type(type), tag(tag), type_given(true), tag_given(true) {}
+            TokenSearch(const TokenType type, const std::string& tag, std::size_t level) : type(type), tag(tag), level(level), type_given(true), tag_given(true), level_given(true){}
+            TokenSearch(const TokenType type, const std::string& tag, const std::size_t level, std::size_t code)
+               : type(type),
+                tag(tag),               
+                level(level),
+                code(code),
+                type_given(true),
+                tag_given(true),
+                level_given(true),
+                code_given(true)
+            { }
+        private:
+            void assignCorrectValue(std::size_t num, NumValue nv)
+            {
+                if (nv == NumValue::LEVEL) {
+                    level = num;
+                    level_given = true;
+                }
+                else {
+                    code = num;
+                    code_given = true;
+                }
+            }
+
+        };
+
+    public:
+        enum class FindType
+        {
+            BEGINNING,
+            END
+        };
+
+    public:
+        TokenCollection() = default;
+        TokenCollection(const std::vector<Token>& tokens)
+        {
+            set(tokens);
+        }
+
+        void clear()
+        {
+            m_tokens.clear();
+        }
+
+        TokenCollection* add(TokenType, const std::string& name, const std::string& attr = "");
+        void set(const std::vector<Token>&);
+
+        it begin() { return m_tokens.begin(); }
+        [[nodiscard]] cit begin() const { return m_tokens.cbegin(); }
+        it end() { return m_tokens.end(); }
+        [[nodiscard]] cit end() const { return m_tokens.cend(); }
+
+        std::vector<Token>& getAllTokens() { return m_tokens; }
+
+        [[nodiscard]] bool empty() const { return m_tokens.empty(); }
+
+        [[nodiscard]] cit findTag(const std::string& tag, FindType mode = FindType::BEGINNING) const { return findTag(m_tokens.begin(), TokenSearch{ tag }, mode); }
+        [[nodiscard]] cit findTag(const std::string& tag, std::size_t level, FindType mode = FindType::BEGINNING) const { return findTag(m_tokens.begin(), TokenSearch{ tag, level }, mode); }
+        [[nodiscard]] cit findTag(const std::string& tag, std::size_t level, std::size_t code, const FindType mode = FindType::BEGINNING) const { return findTag(m_tokens.begin(), TokenSearch{ tag, level , code}, mode); }
+        [[nodiscard]] cit findTag(TokenType type, FindType mode = FindType::BEGINNING) const { return findTag(m_tokens.begin(), TokenSearch{ type }, mode); }
+        [[nodiscard]] cit findTag(TokenType type, const std::string& tag, FindType mode = FindType::BEGINNING) const { return findTag(m_tokens.begin(), TokenSearch{ type, tag }, mode); }
+        [[nodiscard]] cit findTag(TokenType type, const std::string& tag, std::size_t level, FindType mode = FindType::BEGINNING) const
+        {
+            return findTag(m_tokens.begin(), TokenSearch{ type, tag, level }, mode);
+        }
+
+        [[nodiscard]] cit findTag(cit startingPoint, const std::string& tag, FindType mode = FindType::BEGINNING) const { return findTag(startingPoint, TokenSearch{ tag }, mode); }
+        [[nodiscard]] cit findTag(cit startingPoint, const std::string& tag, std::size_t level, FindType mode = FindType::BEGINNING) const { return findTag(startingPoint, TokenSearch{ tag, level }, mode); }
+        [[nodiscard]] cit findTag(cit startingPoint, const std::string& tag, std::size_t level, std::size_t code, const FindType mode = FindType::BEGINNING) const { return findTag(startingPoint, TokenSearch{ tag, level }, mode); }
+        [[nodiscard]] cit findTag(cit startingPoint, TokenType type, FindType mode = FindType::BEGINNING) const { return findTag(startingPoint, TokenSearch{ type }, mode); }
+        [[nodiscard]] cit findTag(cit startingPoint, TokenType type, const std::string& tag, FindType mode = FindType::BEGINNING) const { return findTag(startingPoint, TokenSearch{ type, tag }, mode); }
+        [[nodiscard]] cit findTag(cit startingPoint, TokenType type, const std::string& tag, std::size_t level, FindType mode = FindType::BEGINNING) const { return findTag(startingPoint, TokenSearch{ type, tag, level }, mode); }
+
+        void insert(cit position, const Token&);
+        void insert(cit position, const std::vector<Token>&);
+
+        Token& front() { return m_tokens.front(); }
+        [[nodiscard]] const Token& front() const { return m_tokens.front(); }
+        Token& back() { return m_tokens.back(); }
+        [[nodiscard]] const Token& back() const { return m_tokens.back(); }
+
+    private:
+        [[nodiscard]] cit findTag(cit startingPoint, const TokenSearch&, FindType mode = FindType::BEGINNING) const;
+
+        bool isOpenType(const Token& token) { return token.type == TokenType::TAG_OPEN; }
+        bool isOpenType(TokenType type) { return type == TokenType::TAG_OPEN; }
+        bool isCloseType(const Token& token) { return token.type == TokenType::TAG_CLOSE; }
+        bool isCloseType(TokenType type) { return type == TokenType::TAG_CLOSE; }
+
+        void calculateLevelValues();
+    };
+}
