3a79aa4890dfaf9ef5ac0559c6c22b9e767f490e
AntonioCS <antoniocs@gmail.com>
2020-05-21 15:46:47 +0100

    Changed weight to level and added code and a few more constructors

--- src/HtmlParser/Token.h
+++ src/HtmlParser/Token.h
@@ -19,11 +19,31 @@ namespace HtmlParser
         TokenType type;
         std::string tag{};
         std::string attributes{};
-        std::size_t weight{};
+        std::size_t level{};
+        std::size_t code{};
 
-        Token(const TokenType type, const std::string t) : Token(type, t, "") {}
-        Token(const TokenType type, const std::string t, const std::string& attr) : Token(type, t, attr, 0) {}
-        Token(const TokenType type, const std::string& t, const std::string& attr, std::size_t wght) : type(type), tag(t), attributes(attr), weight(wght){}
+        static const std::size_t level_amount{ 10 };
+
+        Token(const TokenType type, const std::string& t)
+            : Token(type, t, "") {}
+        Token(const TokenType type, const std::string& t, const std::string& attr)
+            : Token(type, t, attr, 0) {}
+        Token(const TokenType type, const std::string& t, std::size_t wght)
+            : Token(type, t,"", wght){}
+        Token(const TokenType type, const std::string& t, std::size_t wght, std::size_t level)
+            : Token(type, t, "", wght, level) {}
+        Token(const TokenType type, const std::string& t, const std::string& attr, std::size_t wght)
+            : type(type), tag(t), attributes(attr), level(wght)
+        {
+            if (type == TokenType::TAG_OPEN || type == TokenType::TAG_CLOSE || type == TokenType::TAG_SELF_CLOSE) {
+                for (const auto& c : tag) {
+                    code += static_cast<std::size_t>(c);
+                }
+            }
+        }
+
+        Token(const TokenType type, const std::string& t, const std::string& attr, std::size_t wght, std::size_t code)
+            : type(type), tag(t), attributes(attr), level(wght), code(code) {}
     };
 
 }
