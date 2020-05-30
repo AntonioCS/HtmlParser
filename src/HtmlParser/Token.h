00a4ce52745584bc54e5f5b5a69942edc6dc18c2
AntonioCS <antoniocs@gmail.com>
2020-05-30 13:40:28 +0100

    Added a few more functions

--- src/HtmlParser/Token.h
+++ src/HtmlParser/Token.h
@@ -35,15 +35,40 @@ namespace HtmlParser
         Token(const TokenType type, const std::string& t, const std::string& attr, std::size_t wght)
             : type(type), tag(t), attributes(attr), level(wght)
         {
-            if (type == TokenType::TAG_OPEN || type == TokenType::TAG_CLOSE || type == TokenType::TAG_SELF_CLOSE) {
-                for (const auto& c : tag) {
-                    code += static_cast<std::size_t>(c);
-                }
+            if (type == TokenType::TAG_OPEN || type == TokenType::TAG_CLOSE) {// || type == TokenType::TAG_SELF_CLOSE) {
+                code = generateTagId(tag);
             }
         }
 
         Token(const TokenType type, const std::string& t, const std::string& attr, std::size_t wght, std::size_t code)
             : type(type), tag(t), attributes(attr), level(wght), code(code) {}
+
+        [[nodiscard]] bool isOpenType() const { return (type == TokenType::TAG_OPEN); }
+        [[nodiscard]] bool isCloseType() const { return (type == TokenType::TAG_CLOSE); }
+        [[nodiscard]] bool isOpenOrCloseType() const { return (isOpenType() || isCloseType()); }
+        [[nodiscard]] bool isTag(const std::string& t) const { return (tag == t); }
+
+        [[nodiscard]] bool isPartner(const Token& token) const
+        {
+            return ((isOpenType() && token.isCloseType() || isCloseType() && token.isOpenType())
+                && code == token.code && level == token.level);
+        }
+
+        [[nodiscard]] Token generateOposite() const
+        {
+            return { (isOpenType() ? TokenType::TAG_CLOSE : TokenType::TAG_OPEN) , tag };
+        }
+
+        static std::size_t generateTagId(const std::string& tag)
+        {
+            std::size_t code{};
+
+            for (const auto& c : tag) {
+                code += static_cast<std::size_t>(c);
+            }
+
+            return code;
+        }
     };
 
 }
