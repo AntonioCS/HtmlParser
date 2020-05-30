33f70ba3b3d457073fd53692b06b40bd69a50d41
AntonioCS <antoniocs@gmail.com>
2020-05-30 13:36:47 +0100

    Got this to work

--- src/HtmlParser/Fixer.cpp
+++ src/HtmlParser/Fixer.cpp
@@ -1,203 +1,150 @@
-﻿#include "Fixer.h"
+﻿#include <HtmlParser/TokenCollection.h>
 #include <filesystem>
-#include <iostream>
-#include <stdexcept>
 #include <utility>
-#include <HtmlParser/TokenCollection.h>
-#include "../tests/TestHelper.h"
+#include <array>
+#include <functional>
+#include <tuple>
+#include "Fixer.h"
+
+
+#define DEBUG_FIXER 0
+#if DEBUG_FIXER
+    #include "../tests/TestHelper.h"
+    #include <iostream>
+#endif
 
 namespace HtmlParser
 {
-
-    void Fixer::process(std::vector<Token>& tokens)
-    {
-        if (!tokens.empty()) {
-            ensureProperStructure(tokens);            
-            analyseAndFix(tokens);
-        }
-    }
-
     void Fixer::process(TokenCollection& tokenCollection)
     {
         if (!tokenCollection.empty()) {
             ensureProperStructure(tokenCollection);
+            analyseAndFix(tokenCollection);
         }
     }
 
-    void Fixer::ensureProperStructure(std::vector<Token>& tokens)
+    void Fixer::ensureProperStructure(TokenCollection& tokenCollection)
     {
-        if (tokens.front().type != TokenType::DOCTYPE) {
-            tokens.insert(tokens.begin(), { TokenType::DOCTYPE,"html" });
-        }
+#if DEBUG_FIXER
+        std::cout << "Ensure Proper structure before:\n";
+        output_tokens(tokenCollection.getAllTokens());
+#endif
 
-        if (!isStartHtmlTag(tokens.at(1))) {
-            tokens.insert(tokens.begin() + 1, { HtmlParser::TokenType::TAG_OPEN, "html" });
-        }
-
-        if (!isEndHtmlTag(tokens.back())) {
-            tokens.emplace_back(HtmlParser::TokenType::TAG_CLOSE, "html");
-        }
-
-        //check for head
-        if (!isTagMatch(tokens.at(2), "head")) {
-            std::vector<Token> basic_head{
-                {TokenType::TAG_OPEN, "head"},
-                    //{TokenType::TAG_OPEN, "title"},
-                    //{TokenType::TAG_CLOSE, "title"},
-                {TokenType::TAG_CLOSE, "head"}
-            };
-
-            tokens.insert(tokens.begin() + 2, basic_head.begin(), basic_head.end());
-        }
-
-        auto body_pos = findTag(tokens, "body");
-        if (body_pos == tokens.end()) {
-            auto head_close_pos = findTag(tokens,"head", -1, TokenType::TAG_CLOSE);
-
-            tokens.insert(head_close_pos + 1, { HtmlParser::TokenType::TAG_OPEN, "body" });
-            tokens.insert(tokens.end() - 1, { HtmlParser::TokenType::TAG_CLOSE, "body" });            
-        }
-    }
-
-    void Fixer::ensureProperStructure(TokenCollection&)
-    {
-    }
-
-    void Fixer::addWeightValues(std::vector<Token>& tokens)
-    {
-        std::size_t weight{0};
-
-        for (auto& token : tokens) {
-            if (isOpenType(token)) {
-                token.level = weight;
-                //weight += m_weight_amount + token.tag.size();
-                weight += m_weight_amount;
-            }
-            else if (isCloseType(token)) {               
-                //weight -= (m_weight_amount + token.tag.size());
-                weight -= m_weight_amount;
-                token.level = weight;
-            }
-            else {
-                token.level = weight;
-            }
-        }
-    }
-
-    void Fixer::analyseAndFix(std::vector<Token>& tokens, std::size_t i)
-    {
-        //@todo: There might be a way to only readd weitghts to the incorrect tags and not redo everything
-        addWeightValues(tokens);
-        
-        std::filesystem::path tokens_file{ std::string{"output_tokens.txt" } };
-
-        OutputCppTokens o{
-            true, false, false, true , {}, {}
-            //{TokenType::TEXT, TokenType::CODE, TokenType::TAG_SELF_CLOSE},
-          //  {"script", "li", "ul", "a", "table", "td", "tr", "i", "b", "span", "strong"}
+        std::array<std::tuple<TokenType, std::string, std::function<void(void)>>, 6> check_for_existance {
+            std::make_tuple(TokenType::DOCTYPE, "doctype", [&tokenCollection]()
+            {
+                tokenCollection.insert(tokenCollection.begin(), { TokenType::DOCTYPE,"doctype", "html" });
+            }),
+            std::make_tuple(TokenType::TAG_OPEN, "html", [&tokenCollection]()
+            {
+                tokenCollection.insert(1, {TokenType::TAG_OPEN,"html"});
+            }),
+            std::make_tuple(TokenType::TAG_CLOSE, "html", [&tokenCollection]()
+            {
+                tokenCollection.insert(tokenCollection.end(), {TokenType::TAG_CLOSE,"html"});
+            }),
+            std::make_tuple(TokenType::TAG_OPEN, "head", [&tokenCollection]()
+            {
+                tokenCollection.insert(
+                    tokenCollection.findTag("html") + 1, 
+                    { {TokenType::TAG_OPEN,"head"}, {TokenType::TAG_CLOSE,"head"} }
+                );
+            }),
+            std::make_tuple(TokenType::TAG_OPEN, "body", [&tokenCollection]()
+            {
+                tokenCollection.insert(
+                    tokenCollection.findTag(TokenType::TAG_CLOSE,"head") + 1,
+                    {TokenType::TAG_OPEN,"body"}
+                );
+            }),
+            std::make_tuple(TokenType::TAG_CLOSE, "body", [&tokenCollection]()
+            {
+                tokenCollection.insert(
+                    tokenCollection.findTag(TokenType::TAG_CLOSE,"html", TokenCollection::FindType::END),
+                    {TokenType::TAG_CLOSE,"body"}
+                );
+            }),
         };
-       //output_cpp_tokens(tokens, "big_file", &tokens_file);
-        o.output(tokens, "big_file", &tokens_file);
 
-        //exit(-1);
-        //*/
-
-        //std::cout << "I = " << i << '\n';
-        //output_tokens(tokens);
-        ////std::cout << "------------------------------------------------------\n";        
-        const auto [body_start_it, body_end_it] = findBodyRange(tokens);
-        bool reCall{ false };
-
-        for (auto it = body_start_it + i; it != body_end_it; it++) {
-            i = std::distance(body_start_it, it);
-            auto& token = *it;
-            std::cout << "Current tag: ";
-            output_token(token);
-            
-            if (isCloseType(token)) {
-                auto res = findTag(tokens, "", it, token.level, TokenType::TAG_OPEN, FindType::END);
-                if (res == tokens.end() || res->tag == "body") {                    
-                    tokens.erase(it);
-                    reCall = true;
-                    break;
-                }
-
-                if (res->tag != token.tag) {                    
-                    tokens.insert(res, { TokenType::TAG_OPEN, token.tag });
-                    reCall = true;
-                    break;
-                }
-            }            
-            else if (isOpenType(token)) {
-                auto res = findTag(tokens, token.tag, it, token.level, TokenType::TAG_CLOSE);
-
-                if (res == tokens.end()) {
-                    if (token.level == m_weight_of_body_first_element) {
-                        tokens.erase(it);
-                    }
-                    else {
-                        res = findTag(tokens, "", it, token.level, TokenType::TAG_CLOSE);
-                        if (res->tag == "html")
-                            res = body_end_it;
-                        tokens.insert(res, { TokenType::TAG_CLOSE, token.tag });
-                    }
-                    reCall = true;
-                    break;
-                }
+        for (const auto& [type, tag, func] : check_for_existance) {
+            auto res = tokenCollection.findTag(type, tag);
+            if (res == tokenCollection.end()) {
+                func();
             }
         }
-
-        if (reCall) {
-            analyseAndFix(tokens, i);
-        }
     }
 
-
-    //std::pair<std::size_t, std::size_t> Fixer::findBodyRange(const std::vector<Token>& tokens)
-    std::pair < Fixer::token_it, Fixer::token_it> Fixer::findBodyRange(const std::vector<Token>& tokens)
+    namespace 
     {
-        auto startIt = findTag(tokens, "body");
-        auto endIt = findTag(tokens, "body", -1, TokenType::TAG_CLOSE, FindType::END);
-        
-        if (startIt == tokens.end() || endIt == tokens.end())
-            throw std::runtime_error{ "Unable to find body" };
-
-        return { startIt,  endIt };
-    }
-
-    //std::vector<Token>::const_iterator Fixer::findTag(const std::vector<Token>& tokens, const std::string& tag, TokenType type)
-    Fixer::token_it Fixer::findTag(
-        const std::vector<Token>& tokens,
-        const std::string& tag,
-        token_it startingPoint,
-        int weight,
-        TokenType type,
-        FindType mode)
-    {
-        auto func = [&tag, &weight, &type](const Token& token)
+        bool interna_analyseAndFix(TokenCollection& tokenCollection, TokenCollection::tc_it& start, TokenCollection::tc_it& end)
         {
-            bool ret{ true };
+#if DEBUG_FIXER
+            std::cout << "//////////////////////////////////////////////////////////////////////////////////////\n";
+            std::cout << "Start token: ";
+            output_token(*start);
+            std::cout << "End token: ";
+            output_token(*end);
+            std::cout << "//////////////////////////////////////////////////////////////////////////////////////\n";
+#endif
+
+            std::vector<Token> open_tokens{};
             
-            if (weight > 0)
-                ret = static_cast<std::size_t>(weight) == token.level;
-            if (!tag.empty())
-                ret = tag == token.tag && ret;
 
-            return (type == token.type && ret);
-        };
+            for (auto it = start; it != end; it++) {
+                auto& token = *it;
+#if DEBUG_FIXER
+                std::cout << "Current tag: ";
+                output_token(token);
+#endif
 
-        if (mode == FindType::BEGINNING)
-            return std::find_if(startingPoint, tokens.end(), func);
+                if (token.isOpenType()) {
+                    open_tokens.push_back(token);
+                }
 
-        auto spoint = tokens.rbegin();
+                else if (token.isCloseType()) {
+                    if (open_tokens.empty()) {
+                        tokenCollection.erase(it);
+                        return true; // call back
+                    }
 
-        if (startingPoint != tokens.begin())
-            spoint += std::distance(startingPoint, tokens.end());
+                    if (!open_tokens.back().isPartner(token)) {
+                        if (tokenCollection.allClosed()) {
+                            token.tag = open_tokens.back().tag;
+                        }
+                        else {
+                            //auto find_closest_opened_tag = tokenCollection.findTag(it, TokenType::TAG_OPEN);
+                            tokenCollection.insert(it, open_tokens.back().generateOposite());
+                            return true; // call back
+                        }
+                    }
 
-        auto res = std::find_if(spoint, tokens.rend(), func);
+                    open_tokens.pop_back();
+                }
+            }
 
-        if (res == tokens.rend()) return tokens.end();
-        return res.base() - 1;
+            return false;
+        }
+    }
+
+
+    void Fixer::analyseAndFix(TokenCollection& tokenCollection, std::size_t i)
+    {
+        bool run{ true };
+
+        while (run) {
+            auto body_open = tokenCollection.findTag("body");
+            auto body_close = tokenCollection.findTag(TokenType::TAG_CLOSE, "body", TokenCollection::FindType::END);
+
+            run = interna_analyseAndFix(tokenCollection, ++body_open, body_close);
+        }
+#if 0
+        OutputCppTokens o{};
+        std::filesystem::path p{ std::string{"token_dump.txt"} };
+        o.output(tokenCollection.getAllTokens(), "tokens", &p);
+
+        //output_tokens(tokenCollection.getAllTokens());
+        exit(-1);
+#endif
     }
 
 }
--- src/HtmlParser/Fixer.h
+++ src/HtmlParser/Fixer.h
@@ -9,60 +9,13 @@ namespace HtmlParser
 
     class Fixer
     {
-        enum class FindType
-        {
-            BEGINNING,
-            END
-        };
-    
-        const std::size_t m_weight_amount{ 10 };
-        const std::size_t m_weight_of_body_first_element{20}; //@NOTE: This might not be correct if elements in HEAD tag are not correct
     public:
         Fixer() = default;
         ~Fixer() = default;
 
-        void process(std::vector<Token>&);
         void process(TokenCollection&);
     private:
-        using token_it = std::vector<Token>::const_iterator;
-
-        void ensureProperStructure(std::vector<Token>&);
         void ensureProperStructure(TokenCollection&);
-
-        void addWeightValues(std::vector<Token>&);
-
-        void analyseAndFix(std::vector<Token>&, std::size_t i = 1);
-        void analyseAndFix(TokenCollection&);
-
-        std::pair<token_it, token_it> findBodyRange(const std::vector<Token>& tokens);
-
-        bool isOpenType(const Token& token) { return token.type == TokenType::TAG_OPEN; }
-        bool isCloseType(const Token& token) { return token.type == TokenType::TAG_CLOSE; }
-        bool isTagMatch(const Token& token, const std::string& match) { return token.tag == match; }
-        bool isStartHtmlTag(const Token& token) { return isOpenType(token) && isTagMatch(token, "html"); }
-        bool isEndHtmlTag(const Token& token) { return isCloseType(token) && isTagMatch(token, "html"); }
-
-        token_it findTag(
-            const std::vector<Token>& tokens,
-            const std::string& tag,
-            int weight = -1,
-            TokenType type = TokenType::TAG_OPEN,
-            FindType mode = FindType::BEGINNING
-        )
-        {
-            return findTag(tokens, tag, tokens.begin(), weight, type, mode);
-        }
-
-        token_it findTag(
-            const std::vector<Token>& tokens,
-            const std::string& tag,
-            token_it,
-            //std::size_t startingPoint = 0, 
-            int weight = -1, 
-            TokenType type = TokenType::TAG_OPEN, 
-            FindType mode = FindType::BEGINNING
-        );
-        
-        
+        void analyseAndFix(TokenCollection&, std::size_t i = 1);
     };
 }
