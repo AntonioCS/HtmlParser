b048e2c6d2a06d36480371c84f4cc56374fc450f
AntonioCS <antoniocs@gmail.com>
2020-05-30 13:52:13 +0100

    Loads of improvements

--- src/HtmlParser/TokenCollection.cpp
+++ src/HtmlParser/TokenCollection.cpp
@@ -1,5 +1,6 @@
 ﻿#include "TokenCollection.h"
 #include <algorithm> //std::find_if
+#include <iostream>
 #include <iterator> //std::distance
 
 //#define DEBUG_TOKENCOLLECTION
@@ -24,33 +25,79 @@ namespace HtmlParser
 
             if (isCloseType(type)) {
                 level -= Token::level_amount;
+                m_total_close++;
             }
         }
-        
+
+        if (isOpenType(type)) m_total_open++;
+
         m_tokens.emplace_back(type, name, attr, level);
 
+        return this;
+    }
+
+    TokenCollection* TokenCollection::add(Token& token)
+    {
+        m_tokens.push_back(token);
         return this;
     }
 
     void TokenCollection::set(const std::vector<Token>& tokens)
     {
         m_tokens = tokens;
-        calculateLevelValues();
+        calculateValues();
     }
 
-    void TokenCollection::insert(const cit position, const Token& token)
+    void TokenCollection::insert(tc_cit position, Token&& token)
     {
         m_tokens.insert(position, token);
-        calculateLevelValues();
+        calculateValues();
     }
 
-    void TokenCollection::insert(const cit position, const std::vector<Token>& tokens)
+    void TokenCollection::insert(std::size_t position, Token&& token)
+    {
+        insert(m_tokens.begin() + position, std::move(token));
+    }
+
+    void TokenCollection::insert(tc_cit position, std::vector<Token>&& tokens)
     {
         m_tokens.insert(position, tokens.begin(), tokens.end());
-        calculateLevelValues();
+        calculateValues();
     }
 
-    TokenCollection::cit TokenCollection::findTag(cit startingPoint, const TokenSearch& tokenSearch, const FindType mode) const
+    void TokenCollection::insert(std::size_t position, std::vector<Token>&& tokens)
+    {
+        insert(m_tokens.begin() + position, std::move(tokens));
+    }
+
+    void TokenCollection::erase(tc_cit position)
+    {
+        m_tokens.erase(position);
+        calculateValues();
+    }
+
+    void TokenCollection::erase(tc_cit start, tc_cit end)
+    {
+        m_tokens.erase(start, end);
+        calculateValues();
+    }
+
+    std::pair<std::size_t, std::size_t> TokenCollection::countOpenCloseTagsBewtween(tc_cit start, tc_cit end) const
+    {
+        std::size_t total_open{};
+        std::size_t total_close{};
+
+        for (auto it = start +1; it != end; it++) {
+            if (it->isOpenType()) 
+                total_open++;
+            else if (it->isCloseType()) 
+                total_close++;
+        }
+
+        return { total_open, total_close };
+    }
+
+    TokenCollection::tc_it TokenCollection::findTag(tc_it startingPoint, const TokenSearch& tokenSearch, const FindType mode)
     {
         auto func = [&tokenSearch](const Token& token)
         {
@@ -74,12 +121,12 @@ namespace HtmlParser
         };
 
         if (mode == FindType::BEGINNING)
-            return std::find_if<cit>(startingPoint,m_tokens.cend(), func);
+            return std::find_if(startingPoint,m_tokens.end(), func);
 
         auto spoint = m_tokens.rbegin();
 
         if (startingPoint != m_tokens.begin())
-            spoint += std::distance <cit>(startingPoint, m_tokens.cend());
+            spoint += std::distance<tc_cit>(startingPoint, m_tokens.end());
 
         auto res = std::find_if(spoint, m_tokens.rend(), func);
 
@@ -87,20 +134,41 @@ namespace HtmlParser
         return res.base() - 1;
     }
 
-    void TokenCollection::calculateLevelValues()
+    void TokenCollection::calculateValues()
     {
         std::size_t level{};
+        m_token_codes.clear();
+        m_total_open = 0;
+        m_total_close = 0;
 
-        for (auto& token : m_tokens) {
+       for (auto& token : m_tokens) {
             token.level = level;
 
-            if (isOpenType(token)) {
+            if (token.isOpenType()) {
                 level += Token::level_amount;
+                m_total_open++;
             }
-            else if (isCloseType(token)) {
-                level -= Token::level_amount;
-                token.level = level;
+
+            else if (token.isCloseType()) {
+                if (token.level != 0) {
+                    level -= Token::level_amount;
+                    token.level = level;
+                }
+                m_total_close++;
             }
         }
+    }
+
+    inline std::vector<std::size_t> TokenCollection::getTokenCodes(tc_it start, tc_it end) const
+    {
+        std::vector<std::size_t> codes{};
+
+        for (auto i = start; i != end; i++) {
+            if (i->isOpenOrCloseType()) {
+                codes.push_back(i->code);
+            }
+        }
+
+        return codes;
     }
 }
